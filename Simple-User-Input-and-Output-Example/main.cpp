b02f55c1ca81d40390015ba05ae664afd3bdfd1f
NPB124 <npb124@gmail.com>
2023-09-13 11:21:57 +0100

    Initial Commit

---
+++ Simple-User-Input-and-Output-Example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Simple-User-Input-and-Output-Example
+//
+//  Created by Nick Brett on 13/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
